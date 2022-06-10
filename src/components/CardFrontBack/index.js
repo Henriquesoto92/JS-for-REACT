d7c73ac1d2649619fd04bab4b0aad8d6c5c40a0c
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-10 09:56:31 -0300

    :sparkles: add click event rotate cardFrontBack

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,9 +2,24 @@ import CardGame from "../CardGame";
 import "./style.css";
 
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest(".card-front-back");
+        //const $cardFrontBack = $origin.parentNode.parentNode.parentNode; // forma manual
+
+        $cardFrontBack.classList.toggle("-active");
+        // o toggle substitui todo esse codigo a baixo
+        // if ($cardFrontBack.classList.contains('.-active')) {
+        //     $cardFrontBack.classList.remove('.-active')
+        // } else {
+        //     $cardFrontBack.classList.add("-active")
+        // }
+        //console.log($cardFrontBack)
+    }
 
     return /*html*/ `
-    <article class="card-front-back">
+    <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
         <div class="card -front">
         ${CardGame()}
         </div>
